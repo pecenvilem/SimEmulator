0afaa926b458598f971d844fa99f9b40a5dedfba
pecenvilem <pecenvilem@gmail.com>
2022-09-26 01:33:55 +0200

    Added '@echo on' into .bat files.

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
+@echo on
+IF exist venv (ren venv venv_old && echo Found existing 'venv' folder. Renamed to 'venv_old')
 echo Creating Python virtual environment for the project...
 python -m venv venv
 
@@ -12,7 +14,7 @@ pip install -r requirements.txt
 echo Deactivating virtual environment...
 call deactivate
 
-echo Installation successfull! Proceed by filling defined credentials in the 'secrets_template.py' file.
+echo Installation successful! Proceed by filling defined credentials in the 'secrets_template.py' file.
 echo Then rename the file to just 'secrets.py'.
 echo You can run the SimEmulator via 'launch.bat' file. See 'configuratin.py' for possible settings.
 echo You can remove the program completely by deleting the root folder. (Where this .bat file is located...)
