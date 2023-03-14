a1c901ebd6bed2c2d6197f8168b06b008a705d2d
pecenvilem <pecenvilem@gmail.com>
2023-03-14 23:04:57 +0100

    Correct Xvfb config in utils/run.sh

--- utils/run.sh
+++ utils/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export DISPLAY=:1
-Xvfb $DISPLAY -screen 0 1920x1080x16 -ac &
+Xvfb $DISPLAY -screen 0 1920x1080x16 &
 fluxbox -display $DISPLAY &
-x11vnc -nevershared -forever &
+x11vnc -display $DISPLAY -nevershared -forever &
 python main.py
