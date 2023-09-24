54814e9492cb27dbb381ea2080be044936265929
veselo21 <veselo21@fit.cvut.cz>
2023-09-24 21:18:51 +0200

    Fix bad balise packet

--- default_balises.py
+++ default_balises.py
@@ -1,44 +1,44 @@
 BALISES = [
     {'position': 400.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 1, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 1, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 402.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 1, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 1, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 1900.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 2, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 2, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 1902.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 2, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 2, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 3400.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 3, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 3, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 3402.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 3, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 3, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 4900.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 4, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 4, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 4902.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 4, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 4, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 6400.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 5, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 5, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 6402.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 5, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 5, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 7900.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 6, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 6, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 7902.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 6, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 6, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 9402.5,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 1, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 7, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 7, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
     {'position': 9400.0,
      'telegram': '{"Q_UPDOWN": 1, "M_VERSION": 16, "Q_MEDIA": 0, "N_PIG": 0, "N_TOTAL": 1, "M_DUP": 0, "M_MCOUNT": 0, '
-                 '"NID_C": 0, "NID_BG": 7, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255}}'},
+                 '"NID_C": 0, "NID_BG": 7, "Q_LINK": 0, "End_Of_Information": {"NID_PACKET": 255, "L_PACKET":0}}'},
 ]
